92aa03042397cc4f0a02c4220e651cfc0d209b16
masarwy-m <mohammad.m@campus.technion.ac.il>
2023-08-12 21:09:56 +0300

    First skeleton

--- scripts/framework.py
+++ scripts/framework.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+
 import yaml
+import rospy
+from grasp_voa.msg import Point2D
+from grasp_voa.srv import ProcessedPoints
+from ssd import compute_combined_rmse
 
 
 class Alg:
@@ -7,8 +13,14 @@ class Alg:
             yaml_data = yaml.safe_load(yaml_file)
         self.possible_grasps = list(yaml_data.keys())
         self.possible_poses = list(list(yaml_data.values())[0].keys())
+        self.pose_belief = {}
+
+        rospy.wait_for_service('/lidar_processed_readings')
+        points = rospy.ServiceProxy('/lidar_processed_readings', ProcessedPoints)
+        for pose in self.possible_poses:
+            self.pose_belief[pose] = compute_combined_rmse(...)
 
 
 if __name__ == '__main__':
+    rospy.init_node('main_node')
     gs = Alg('../config/grasp_score/mug.yaml')
-    print('ok')
